55b568a93a07e77ad1890a520a5b5415fb016e48
VIRTUAL_EXISTENCE <Anil.Maharjan@gmail.com>
2022-03-14 20:46:29 -0500

    Added the APIGateway and Resilience
    Added the APIGateway and Resilience

---
+++ WebStore/APIGateway/src/main/java/swa/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,26 @@
+package swa.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
+
+@RestController
+class FallbackController{
+    @GetMapping("product-fallback")
+    Flux<Void> getFallback()
+    {
+        System.out.println("Fallback occured");
+        return Flux.empty();
+    }
+}
